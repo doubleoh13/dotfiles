d0a1634dae164e876a262b06af4b733f63722943
doubleoh13 <doubleoh13@doubleoh13.dev>
2025-01-18 09:32:45 -0500

    update output and dry-run by default

--- install.ps1
+++ install.ps1
@@ -8,17 +8,17 @@
 # - Auto-elevates itself in Windows
 
 param (
-    [switch] $dryRun,
+    [switch] $Execute,
     [switch] $wasElevated
 )
 
 # Windows requires elevated permissions to create the junctions, so let's elevate if necessary
-if ($IsWindows -and !$dryRun) {
+if ($IsWindows -and $Execute) {
     # Check if the script is already running with elevated privileges
     if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
         Write-Host "This script requires administrative privileges. Restarting with elevation..." -ForegroundColor Yellow
         # Relaunch the script with elevation
-        Start-Process -FilePath "$PSHome\pwsh.exe" -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`" -wasElevated" -Verb RunAs
+        Start-Process -FilePath "$PSHome\pwsh.exe" -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`" -Execute -wasElevated" -Verb RunAs
         exit
     }
 }
@@ -30,8 +30,21 @@ if ($IsWindows) {
     $env:MYDOCUMENTS = $documentsPath
 }
 
-function log($msg) {
-    Write-Host "💡 $msg"
+function log($msg, $type = "info") {
+    switch ($type) {
+        'info' {
+            Write-Host "  $msg" -ForegroundColor White
+        }
+        'check' {
+            Write-Host "  $msg" -ForegroundColor Cyan
+        }
+        'skip' {
+            Write-Host "* $msg" -ForegroundColor Yellow
+        }
+        'link' {
+            Write-Host "+ $msg" -ForegroundColor Green
+        }
+    }
 }
 function Resolve-PathSafe($path) {
     $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath($Path)
@@ -61,7 +74,7 @@ function checkConfig($os, $pathName) {
 }
 
 function determinePath($path) {
-    log "Checking $path..."
+    log "Checking $path..." "check"
 
     $pathName = $path.Name
 
@@ -78,23 +91,23 @@ function determinePath($path) {
 
 function linkFile($map) {
     if (Test-Path $map.Link) {
-        log "Skipping $($map.Link) as it already exists"
+        log "Skipping $($map.Link) as it already exists" "skip"
         return
     }
 
     # validate that $map.Target's directory exists and create it if it doesn't
     $targetDirectory = Split-Path $map.Target
     if (-not (Test-Path $targetDirectory)) {
-        if ($dryRun) {
-            log "Creating directory $targetDirectory"
+        if (!$Execute) {
+            log "Creating directory $targetDirectory" "info"
         }
         else {
             New-Item -ItemType Directory -Path $targetDirectory
         }
     }
 
-    if ($dryRun) {
-        log "Linking $($map.Link) to $($map.Target)"
+    if (!$Execute) {
+        log "Linking $($map.Link) to $($map.Target)" "link"
     }
     else {
         if ((Get-Item $map.Target) -is [System.IO.DirectoryInfo] -and $IsWindows) {
@@ -127,6 +140,10 @@ function main {
 
 try {
     Push-Location $PSScriptRoot
+
+    if (!$Execute) {
+        Write-Host "Performing Dry Run. No changes will be make. To persist changes, rerun with -Execute"
+    }
     main
 
     if ($wasElevated) {
